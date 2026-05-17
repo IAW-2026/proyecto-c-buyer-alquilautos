c2c96b3cef9ca70c4851e93167403a5d01b12ca9
Joaquín Tomás Gutiérrez <161380627+JoaquinGutierrez26@users.noreply.github.com>
2026-05-17 20:41:16 -0300

    Hotfix para deploy

--- alquilautos-buyer/prisma.config.ts
+++ alquilautos-buyer/prisma.config.ts
@@ -1,8 +1,4 @@
 import { defineConfig } from '@prisma/config';
-import * as dotenv from 'dotenv';
-
-// Cargamos manualmente el archivo de Next.js
-dotenv.config({ path: '.env.local' });
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
